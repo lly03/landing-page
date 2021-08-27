81211083c9d5e50b7eefbcf0d4b2a8160f7df059
LucindaLy <lucindaly.ll@gmail.com>
2021-08-27 11:35:34 +1000

    update changes

--- js/app.js
+++ js/app.js
@@ -48,8 +48,8 @@ const anchors = document.querySelectorAll('a');
 /**
  * this functions -> adds class 'active' to each section and the corresponding nav when it reaches the top of viewport 
  * go through every individual section
- * get the y coordinates
- * if the are coordinates are at these positions, set the class to active for the section, otherwise don't
+ * if the section's top is greater than or equal to 0, and the distance from the section's bottom is less than or equal to the height of the viewport
+ * then set the class to active for the section, otherwise don't
  * go through every individual anchor
  * get href value from the anchor
  * if the href value equals to the section id then set class to active for navigation menu, otherwise don't
@@ -58,9 +58,9 @@ const anchors = document.querySelectorAll('a');
 const setToActive = () =>{
     for (const section of sections){
 
-        const box = section.getBoundingClientRect();
+        const rect = section.getBoundingClientRect();
 
-        if(box.y<=150 && box.bottom>=150){
+        if(rect.y>=0 && rect.bottom<=(window.innerHeight || document.documentElement.clientHeight)){
             section.classList.add('your-active-class');
 
             anchors.forEach((anchor) =>{
@@ -73,7 +73,7 @@ const setToActive = () =>{
                 }
             })
        }else{
-        section.classList.remove('your-active-class');
+            section.classList.remove('your-active-class');
         }
     }
 }
@@ -85,16 +85,16 @@ document.addEventListener('scroll', () =>{setToActive();});
 
 /** this function -> scroll to anchor ID using scrollTO event
  * go through each anchor using the ForEach Loop
- * remove the default action of the page automatically going to that section/area
- * get href value from anchor
- * go to the id of sectionId by smoothly scrolling to that area
+ * remove the default action of the page automatically going to that section/area // we want a smooth scroll
+ * assign a const with -> getting the id of #section{$id} by extracting the href value from <a> (because its the same element name)
+ * smoothly scroll to that section Id
  */
 
 const scrollToAnchor = () =>{
         anchors.forEach((anchor) => anchor.addEventListener('click',function(event){
             event.preventDefault();
-            const sectionId = anchor.getAttribute('href').slice(1);
-            document.getElementById(sectionId).scrollIntoView({behavior:'smooth'});
+            const sectionId = document.getElementById(anchor.getAttribute('href').slice(1));
+            sectionId.scrollIntoView({behavior:'smooth'});
         })
     );
 } 
